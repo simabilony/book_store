ce8a1eedfd0ba464284aaa0f661f1813fb05373b
simabilony <simabilony@gmail.com>
2024-08-14 03:53:05 +0300

    versioningApiRout

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,52 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout')->middleware('auth:api');
-    Route::post('refresh', 'refresh')->middleware('auth:api');
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::prefix('v1')->group(function () {
+    /**
+     * Auth Routes
+     *
+     * These routes handle user authentication, including login, registration, and logout.
+     */
+    Route::controller(AuthController::class)->group(function () {
+        /**
+         * Login Route
+         *
+         * @method POST
+         * @route /v1/login
+         * @desc Authenticates a user and returns a JWT token.
+         */
+        Route::post('login', 'login');
+        /**
+         * Register Route
+         *
+         * @method POST
+         * @route /v1/register
+         * @desc Registers a new user and returns a JWT token.
+         */
+        Route::post('register', 'register');
+        /**
+         * Logout Route
+         *
+         * @method POST
+         * @route /v1/logout
+         * @desc Logs out the authenticated user.
+         * @middleware auth:api
+         */
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
+    });
 });
